23753c64183510ff0b98ca7924143cc5055d150d
Oscar Storbacka <ostorbacka@gmail.com>
2018-01-27 01:28:59 +0200

    Introduce data analysis function and pass it to index view

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const sanitize = require('express-validator/filter');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Weather' });
+  res.render('index', { title: 'Weather', data: findTemperatures('helsinki') });
 });
 
 router.post('/api/v1/weather', (req, res) => {
@@ -21,7 +21,10 @@ router.post('/api/v1/weather', (req, res) => {
 });
 
 router.get('/api/v1/weather', (req, res) => {
-  const text = "SELECT * FROM temperatures WHERE location = '" + req.query.location.toLowerCase() + "'";
+  if
+  const text = "SELECT * \
+                FROM temperatures \
+                WHERE location = '" + req.query.location.toLowerCase() + "'"; 
   let results = {};
   db.query(text)
     .then(result => {
@@ -34,3 +37,41 @@ router.get('/api/v1/weather', (req, res) => {
 });
 
 module.exports = router;
+
+function findTemperatures(location) {
+  const maxTemperatureQuery = "SELECT max(temperature) \
+                               FROM temperatures \
+                               WHERE location = '" + location +"' \
+                               AND time > NOW() - interval '1 day'";
+
+  const minTemperatureQuery = "SELECT min(temperature) \
+                               FROM temperatures \
+                               WHERE location = '" + location +"' \
+                               AND time > NOW() - interval '1 day'";
+
+  const currentTemperatureQuery = "SELECT temperature \
+                                  FROM temperatures \
+                                  WHERE location = '" + location +"' \
+                                  ORDER BY date DESC LIMIT 1'";
+
+  let temperatures {location: {'current': null, 'hi': null, 'low': null}}
+  
+  let databaseQuery = new Promise((resolve, reject) => {
+    db.query(currentTemperatureQuery).then(res => {
+    console.log(res.rows);
+    temperatures.location.current = res.rows[0];
+    }).catch(err => console.error('error executing query', err.stack));
+
+    db.query(minTemperatureQuery).then(res => {
+      console.log(res.rows);
+      temperatures.location.low = res.rows[0];
+    }).catch(err => console.error('error executing query', err.stack));
+
+    db.query(maxTemperatureQuery).then(res => {
+      console.log(res.rows);
+      temperatures.location.max = res.rows[0];
+    }).catch(err => console.error('error executing query', err.stack));
+  }
+
+  databaseQuery.then(resolve => { return temperatures; });
+}
